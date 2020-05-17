409693144e9708ade6e160bd5d96a7ee6536ca45
Pan Artur <artur.pankov.16@list.ru>
2020-05-17 23:33:50 +0300

    feat: add some solutions

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -32,7 +32,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  return /\{[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}\}/i;
 }
 
 
@@ -54,7 +54,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return /p.t/;
 }
 
 
